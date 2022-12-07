33e5591308754f3132a4a366060c6629a3e8c28c
neoribello <46947156+neoribello@users.noreply.github.com>
2022-12-07 14:27:12 -0800

    Added data and navbar

---
+++ data/data.js
@@ -0,0 +1,87 @@
+const userData = {
+  githubUsername: "neoribello",
+  name: "Neil Oribello",
+  designation: "Frontend Developer",
+  avatarUrl: "/avatar.png",
+  email: "neil.oribello@gmail.com",
+  phone: "7782236406",
+  address: "Vancouver, BC",
+  projects: [
+    {
+      title: "Project 1",
+      link: "test.com",
+      imgUrl: "/public/vercel.svg",
+    },
+    {
+      title: "Project 2",
+      link: "test.com",
+      imgUrl: "/public/vercel.svg",
+    },
+    {
+      title: "Project 3",
+      link: "test.com",
+      imgUrl: "/public/vercel.svg",
+    },
+    {
+      title: "Project 4",
+      link: "test.com",
+      imgUrl: "/public/vercel.svg",
+    },
+    {
+      title: "Project 5",
+      link: "test.com",
+      imgUrl: "/public/vercel.svg",
+    },
+    {
+      title: "Project 1",
+      link: "test.com",
+      imgUrl: "/public/vercel.svg",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications",
+    description: [
+      `A web developer with an interest and passion in building websites. For the past two years I have pivoted my career from being a web designer to a web developer. With experience working in the tech field, I am eager and willing to collaborate on new projects.`,
+      `I'm enthusiastic about the work that I do, but even more so devoted to help create a positive and efficient environment. `,
+    ],
+  },
+  experience: [
+    {
+      title: "Frontend Developer",
+      company: "TalentMarketplace",
+      year: "2021-2022",
+      companyLink: "https://talentmarketplace.com",
+      desc: "Developed and designed various React projects throughout the TalentMarketplace website",
+    },
+    {
+      title: "Web Designer/Developer",
+      company: "Real Estate Webmasters",
+      year: "2019-2020",
+      companyLink: "https://realestatewebmasters.com/",
+      desc: "Designed websites for top 1% real estate professionals across the United States and Canada",
+    },
+    {
+      title: "Graduation - Diploma",
+      company: "Lighthouse Labs",
+      year: "2021",
+      companyLink: "https://lighthouselabs.ca",
+      desc: "Relevant courses: Databases & PSQL, Software Architecture, JavaScript & Node.js, HTML, CSS, React",
+    },
+    {
+      title: "Graduation - Diploma",
+      company: "British Columbia Institute of Technology",
+      year: "2019",
+      companyLink: "https://bcit.ca",
+      desc: "Had a GPA over 3.5, graduated with distinction",
+    },
+  ],
+  resumeUrl:
+    "https://www.linkedin.com/in/neil-oribello-65a724139/overlay/1635492360598/single-media-viewer/",
+  socialLinks: {
+    linkedin: "https://www.linkedin.com/in/neil-oribello-65a724139/",
+    github: "https://github.com/neoribello",
+  },
+};
+
+export default userData;
